1d531b5b8697a23585adec1a5a83b7c9acb78343
celoxgroup <42559182+celoxgroup@users.noreply.github.com>
2022-03-15 20:49:23 +1100

    Update edulabbtn.php

--- lang/en/edulabbtn.php
+++ lang/en/edulabbtn.php
@@ -22,18 +22,18 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'EduLAB Button';
-$string['modulenameplural'] = 'EduLAB Buttons';
+$string['pluginname'] = 'eduLAB Button';
+$string['modulenameplural'] = 'eduLAB Buttons';
 
-$string['pluginadministration'] = 'EduLAB module administration';
+$string['pluginadministration'] = 'eduLAB module administration';
 
-$string['modulename'] = 'EduLAB Button';
+$string['modulename'] = 'eduLAB Button';
 //$string['modulename_help'] = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla blandit cursus libero sed gravida. Donec non velit consequat, fermentum mauris at, aliquet eros. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc volutpat viverra libero, nec semper augue varius posuere. Integer orci arcu, mattis vel imperdiet nec, convallis ac eros. Fusce mollis condimentum odio, et bibendum urna tempus vitae. Pellentesque congue, lectus et vulputate lacinia, elit metus vulputate elit, at scelerisque ipsum dui at felis. Vestibulum id arcu orci. Praesent pellentesque et augue ut egestas. Sed sit amet tempus ipsum. Maecenas sit amet malesuada mi. Phasellus et placerat odio. Aliquam ac nibh id ex tristique mattis in ac massa.
 //
 //Proin neque enim, suscipit vitae leo vel, fermentum semper nisi. Nunc sit amet neque quis tellus viverra cursus sed non leo. Etiam accumsan neque mattis magna efficitur vehicula. Cras ut interdum magna. Nullam ut lacus nec libero cursus bibendum. Quisque et interdum dui, id vehicula felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam gravida, nulla id placerat consequat, leo mauris condimentum nulla, eu consectetur ex quam non dolor. Mauris sed viverra ex.';
 $string['modulename_help'] = '';
 
-$string['edulabbtn:viewall'] = 'View EduLAB Button';
+$string['edulabbtn:viewall'] = 'View eduLAB Button';
 $string['lab_id'] = 'Lab ID';
 
 $string['auth_method:local'] = 'Local';
